45d88a20011b4a83172caba167c598a8afff6a42
Paulo Afonso <pauloferrisouza@gmail.com>
2021-12-31 18:13:47 -0300

    exercicios 1.2-4

--- fundamentos/dia-3-jest/sum.test.js
+++ fundamentos/dia-3-jest/sum.test.js
@@ -4,4 +4,13 @@ describe('sum', () => {
   test('retorno de sum(4, 5) é 9', () =>{
     expect(sum(4,5)).toBe(9);
   })
+  test('retorno de sum(0,0) é 0', () =>{
+    expect(sum(0,0)).toBe(0);
+  })
+  test('erro quando parâmetros não são números', () =>{
+    expect(() => sum(4,"5")).toThrow(Error);
+  })
+  test('mensagem de erro é "parameters must be numbers"', () => {
+    expect(() => sum(4,"5")).toThrow('parameters must be numbers')
+  })
 })
