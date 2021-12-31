9b31ce5362c4bc27152fc9d2fb45bdc9260ec519
Paulo Afonso <pauloferrisouza@gmail.com>
2021-12-31 19:07:14 -0300

    exercicio 3

--- fundamentos/dia-3-jest/sum.test.js
+++ fundamentos/dia-3-jest/sum.test.js
@@ -16,16 +16,37 @@
 // })
 
 //exercicio 2
-const myRemove = require('./myRemove.js')
+// const myRemove = require('./myRemove.js')
 
-describe('myRemove', () =>{
-  test('[1, 2, 3, 4], 3 retorna array esperado', () =>{
-    expect(myRemove([1,2,3,4],3)).toEqual(expect.arrayContaining([1,2,4]));
+// describe('myRemove', () =>{
+//   test('[1, 2, 3, 4], 3 retorna array esperado', () =>{
+//     expect(myRemove([1,2,3,4],3)).toEqual(expect.arrayContaining([1,2,4]));
+//   })
+//   test('[1,2,3,4], 3 nao retornar [1,2,3,4]', () => {
+//     expect(myRemove([1,2,3,4],3)).not.toEqual(expect.arrayContaining([1,2,3,4]));
+//   })
+//   test('[1,2,3,4],5 retorna array esperado', () => {
+//     expect(myRemove([1,2,3,4],5)).toEqual(expect.arrayContaining([1,2,3,4]));
+//   })
+// })
+
+//exercicio 3
+const myFizzBuzz = require('./myFizzBuzz.js')
+
+describe('myFizzBuzz', () => {
+  test('número é divisível por 3 e 5', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
   })
-  test('[1,2,3,4], 3 nao retornar [1,2,3,4]', () => {
-    expect(myRemove([1,2,3,4],3)).not.toEqual(expect.arrayContaining([1,2,3,4]));
+  test('número divisível somente por 3', () => {
+    expect(myFizzBuzz(3)).toEqual('fizz');
   })
-  test('[1,2,3,4],5 retorna array esperado', () => {
-    expect(myRemove([1,2,3,4],5)).toEqual(expect.arrayContaining([1,2,3,4]));
+  test('número divisível somente por 5', () => {
+    expect(myFizzBuzz(5)).toEqual('buzz');
+  })
+  test('número não é divisível por 3 ou 5', () => {
+    expect(myFizzBuzz(2)).toEqual(2);
+  })
+  test('parâmetro não é um número', () => {
+    expect(myFizzBuzz('s')).toEqual(false);
   })
 })
